2a103056d097d154fb8fa36c53cd4de7dd6f1b72
Zander Pantelick <zpantelick@gmail.com>
2025-01-13 15:48:23 -0800

    Added doc strings to app/models/user and app/models/tickets

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,20 @@ class User < ApplicationRecord
   validates_presence_of :password, on: :create
   validates_length_of :password, minimum: 7, maximum: 255, on: :create
 
+# Class Definition: set_default_role
+# ----------------------------------
+# Sets the default role of the user to “organization”
+#
+# @return [Symbol] the default role of the user
   def set_default_role
     self.role ||= :organization
   end
 
+# Class Definition: to_s
+# -----------------------
+# Modifies the built in function to_s to return the email associated
+#
+# @return [String] the email of the user
   def to_s
     email
   end

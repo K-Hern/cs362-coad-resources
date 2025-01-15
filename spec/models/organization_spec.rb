c3de5386ee4d6d40f37eeba378c9338d8cd289d9
Zander Pantelick <zpantelick@gmail.com>
2025-01-15 14:32:12 -0800

    updated organization tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+    let (:org) { Organization.new }
+
+    it "exists" do
+        Organization.new
+    end
+
+    it "responds to name" do
+        expect(org).to respond_to(:name)
+    end
+
+    it "responds to status" do
+        expect(org).to respond_to(:status)
+    end
 end

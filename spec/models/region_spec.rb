672e1f33e2dfd3a9460504368ef44853cd8ee8e9
Kevin Hernandez <KFHEDU@gmail.com>
2025-01-17 16:49:08 -0800

    Finished Specs for Region & Resource Category DB attributes & associations

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,15 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  let(:region) {Region.new()}
+
+  # To be Deleted
   it "exists" do
     Region.new
   end
 
+  # Attribute Testing
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
 
+  # Association testing
+  it  "has many tickets" do
+    should have_many(:tickets)
+  end
+
+  it  "has a name between 1 & 255 chars" do
+    should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+  end
+
+  it  "has a unique and non case sensitive name" do
+    should validate_uniqueness_of(:name).case_insensitive
+  end
+
+  # Tests concerning the class definition
   it "has a string representation that is its name" do
     name = 'Mt. Hood'
     region = Region.new(name: name)
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,35 +2,59 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let(:resourceCat) {ResourceCategory.new(name: "Shelter")}
+
+  # To be deleted
   it "exists" do
     ResourceCategory.new
   end
 
+  #Attribute Testing
   it "has a name" do
-    resourceCat = ResourceCategory.new
     expect(resourceCat).to respond_to(:name)
   end
 
+  it "has a status" do
+    expect(resourceCat).to respond_to(:active)
+  end
+
+  # Association Testing
+  it "has and belongs to many organizations" do
+    should have_and_belong_to_many(:organizations)
+  end
+
+  it "has many tickets" do
+    should have_many(:tickets)
+  end
+
+  it "has a name" do
+    should validate_presence_of(:name)
+  end
+
+  it "has a name with between 1 - 255 chars" do
+    should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+  end
+
+  it "has a unique, non case sensitive name" do
+    should validate_uniqueness_of(:name).case_insensitive()
+  end
+
+  # These are tests concerning the attributes of the class definition (To Be deleted)
   it "has a string representation that is its name" do
-    name = 'Shelter'
-    resourceCat = ResourceCategory.new(name: name)
     result = resourceCat.to_s
+    expect(result).to eq("Shelter")
   end
 
   it "has a name with length between 1 and 255" do
-    name = 'Shelter'
-    resourceCat = ResourceCategory.new(name: name)
     result = resourceCat.to_s
     expect(result.length).to be_between(1, 255)
   end
 
   it "has a boolean status flag" do
-    resourceCat = ResourceCategory.new()
     expect(resourceCat).to respond_to(:active)
   end
 
   it "has a way to check active status" do
-    resourceCat = ResourceCategory.new
     expect(resourceCat).to respond_to(:inactive?)
   end
 
@@ -39,7 +63,4 @@ RSpec.describe ResourceCategory, type: :model do
     resourceCat.activate()
     expect(resourceCat.inactive?).to be_falsey()
   end
-
-
-
 end

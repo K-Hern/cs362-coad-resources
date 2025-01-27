a181d0d76e596510f6c1ebeb743030c021b0f5af
Zander Pantelick <zpantelick@gmail.com>
2025-01-27 14:31:34 -0800

    updated organization_spec to include validation, belong to, and attribute tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,59 +2,142 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-    describe "organizations" do
-        let (:org) { Organization.new }
+    describe "organizations:" do
+        let (:org) { Organization.new() }
+        
 
         it "exists" do
             Organization.new
         end
 
-        it "responds to name" do
-            expect(org).to respond_to(:name)
+        describe "validation tests:" do
+            describe "validates presence of:" do
+                
+                it "email" do
+                    expect(org).to validate_presence_of(:email)
+                end
+
+                it "name" do
+                    expect(org).to validate_presence_of(:name)
+                end
+
+                it "phone" do
+                    expect(org).to validate_presence_of(:phone)
+                end
+
+                it "status" do
+                    expect(org).to validate_presence_of(:status)
+                end
+
+                it "primary_name" do
+                    expect(org).to validate_presence_of(:primary_name)
+                end
+
+                it "secondary_name" do
+                    expect(org).to validate_presence_of(:secondary_name)
+                end
+
+                it "secondary_phone" do
+                    expect(org).to validate_presence_of(:secondary_phone)
+                end
+            end
+
+            describe "validates length of:" do
+                it "email" do
+                    expect(org).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+                end
+                
+                it "name" do
+                    expect(org).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+                end
+
+                it "description" do
+                    expect(org).to validate_length_of(:description).is_at_most(1020).on(:create)
+                end
+            end
+
+            describe "validates uniqueness of:" do
+                it "email" do
+                    expect(org).to validate_uniqueness_of(:email).case_insensitive
+                end
+
+                it "name" do
+                    expect(org).to validate_uniqueness_of(:name).case_insensitive
+                end
+            end
+
+            describe "validates format of:" do
+                it "email" do
+                    org.email = "test@email.com"
+                    pp org
+                    expect(org.email).to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+                end
+            end
         end
 
-        it "responds to status" do
-            expect(org).to respond_to(:status)
+        describe "belongs to tests" do
+            it "has many users" do
+                should have_many(:users)
+            end
+
+            it "has many tickets" do
+                should have_many(:tickets)
+            end
+
+            it "has and belongs to many resource categories" do
+                should have_and_belong_to_many(:resource_categories)
+            end
         end
 
-        it "responds to phone" do
-            expect(org).to respond_to(:phone)
-        end
         
-        it "responds to email" do
-            expect(org).to respond_to(:email)
-        end
+        describe "attribute tests" do
+            it "responds to name" do
+                expect(org).to respond_to(:name)
+            end
 
-        it "responds to description" do
-            expect(org).to respond_to(:description)
-        end
+            it "responds to status" do
+                expect(org).to respond_to(:status)
+            end
 
-        it "responds to rejection_reason" do
-            expect(org).to respond_to(:rejection_reason)
-        end
+            it "responds to phone" do
+                expect(org).to respond_to(:phone)
+            end
+            
+            it "responds to email" do
+                expect(org).to respond_to(:email)
+            end
 
-        it "responds to liability_insurance" do
-            expect(org).to respond_to(:liability_insurance)
-        end
+            it "responds to description" do
+                expect(org).to respond_to(:description)
+            end
 
-        it "responds to primary_name" do
-            expect(org).to respond_to(:primary_name)
-        end
+            it "responds to rejection_reason" do
+                expect(org).to respond_to(:rejection_reason)
+            end
 
-        it "responds to secondary_name" do
-            expect(org).to respond_to(:secondary_name)
-        end
+            it "responds to liability_insurance" do
+                expect(org).to respond_to(:liability_insurance)
+            end
 
-        it "responds to secondary_phone" do
-            expect(org).to respond_to(:secondary_phone)
-        end
+            it "responds to primary_name" do
+                expect(org).to respond_to(:primary_name)
+            end
 
-        it "responds to title" do
-            expect(org).to respond_to(:title)
-        end
+            it "responds to secondary_name" do
+                expect(org).to respond_to(:secondary_name)
+            end
 
-        it "responds to transportation" do
-            expect(org).to respond_to(:transportation)
+            it "responds to secondary_phone" do
+                expect(org).to respond_to(:secondary_phone)
+            end
+
+            it "responds to title" do
+                expect(org).to respond_to(:title)
+            end
+
+            it "responds to transportation" do
+                expect(org).to respond_to(:transportation)
+            end
         end
 
         describe "status" do

6a564393b3d0106750ae71826f3b4c842531a0c6
Kevin Hernandez <KFHEDU@gmail.com>
2025-01-27 20:05:11 -0800

    Added Dashboard Helper Specs

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,5 +11,48 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
+  describe "dashboard_for" do
+
+    before(:all) do
+      @orgObj =
+        Organization.new(
+          name: "example organization",
+          email: "example@example.com",
+          phone: "+1 541-567-0972",
+          primary_name: "Example Org",
+          secondary_name: "Example Org2",
+          secondary_phone: "+1 541-826-9832"
+        )
+
+      @user = User.new(
+        email: "example@exmaple.com",
+        password: "This is a test",
+        role: :organization,
+        organization: @orgObj
+        )
+    end
+
+    it "Tailors to admin Users" do
+      @user.role = :admin
+      expect(dashboard_for(@user)).to eq('admin_dashboard')
+    end
+
+    it "Tailors to Organizations w/ submissions" do
+      @user.role = :organization # Needed due to short circuiting
+      expect(dashboard_for(@user)).to eq('organization_submitted_dashboard')
+    end
+
+    it "Tailors to Organizations w/ approved submissions" do
+      @user.role = :organization # Needed due to short circuiting
+      @orgObj.approve
+      expect(dashboard_for(@user)).to eq('organization_approved_dashboard')
+    end
+
+    it "Defaults to create a new application" do
+      @user.role = :organization # Needed due to short circuiting
+      @user.organization = nil
+      expect(dashboard_for(@user)).to eq('create_application_dashboard')
+    end
+  end
 
 end

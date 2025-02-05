2fe3128deddbf2b08319bc60f43437788fb630ca
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-02-05 14:50:01 -0800

    Added factory for organization

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
 
     factory :organization do
         name { "Organization Name" }
+        email { "test@email.com" }
+        phone {"541-280-2937"}
+        status {"submitted"}
+        primary_name {"uniquename"}
+        secondary_name {"test"}
+        secondary_phone {"123-123-1234"}
     end
     
 end

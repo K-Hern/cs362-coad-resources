9cde52d9547260d59d80b553de07dc0f682ef90e
Zander Pantelick <zpantelick@gmail.com>
2025-02-12 19:39:54 -0800

    Add tests for resource_categories_controller for non mocked routes

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -12,5 +12,76 @@ require 'rails_helper'
 # DELETE /resource_categories/:id(.:format)                   resource_categories#destroy
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+    describe "Ticket Controller" do
+        describe "Logged Out:" do
+            let(:user) { FactoryBot.create(:user) }
 
+            describe "GET - /resource_categories(.:format) - resource_categories#index" do
+                it {
+                    expect(get(:index)).to redirect_to new_user_session_path
+                }
+            end
+
+            describe "POST - /resource_categories(.:format) - resource_categories#create" do
+                it {
+                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+                    expect(response).to redirect_to new_user_session_path
+                }
+            end
+
+            describe "GET - /resource_categories(.:format) - resource_categories#new" do
+                it { 
+                    expect(get(:new)).to redirect_to new_user_session_path
+                }
+            end
+        end
+
+        describe "Logged in as User:" do
+            let(:user) { FactoryBot.create(:user) }
+            before(:each) { sign_in user }
+
+            describe "GET - /resource_categories(.:format) - resource_categories#index" do
+                it {
+                    expect(get(:index)).to redirect_to dashboard_path
+                }
+            end
+
+            describe "POST - /resource_categories(.:format) - resource_categories#create" do
+                it {
+                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+                    expect(response).to redirect_to dashboard_path
+                }
+            end
+
+            describe "GET - /resource_categories(.:format) - resource_categories#new" do
+                it { 
+                    expect(get(:new)).to redirect_to dashboard_path
+                }
+            end
+        end
+
+        describe "Logged in as Admin:" do
+            let(:user) { FactoryBot.create(:user, :admin) }
+            before(:each) { sign_in user }
+
+            describe "GET - /resource_categories(.:format) - resource_categories#index" do
+                it {
+                    expect(get(:index)).to be_successful
+                }
+            end
+
+            describe "POST - /resource_categories(.:format) - resource_categories#create" do
+                it {
+                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+                    expect(response).to redirect_to resource_categories_path
+                }
+            end
+
+            describe "GET - /resource_categories(.:format) - resource_categories#new" do
+                it { 
+                    expect(get(:new)).to be_successful
+                }
+            end
+        end
+    end
 end

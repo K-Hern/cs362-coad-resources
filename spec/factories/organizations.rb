1321466617e91cb8554adbff500ee38494acd277
Zander Pantelick <zpantelick@gmail.com>
2025-02-12 18:40:13 -0800

    Updated organization factory to use sequences and add status traits

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,13 +1,24 @@
 FactoryBot.define do
 
     factory :organization do
-        name { "Organization Name" }
-        email { "test@email.com" }
-        phone {"541-280-2937"}
-        status {"submitted"}
-        primary_name {"uniquename"}
-        secondary_name {"test"}
-        secondary_phone {"123-123-1234"}
+        name  # defined as a sequence
+        email  # defined as a sequence
+        phone  # defined as a sequence
+        primary_name { name }  # defined as a sequence
+        secondary_name { name }  # defined as a sequence
+        secondary_phone { phone }  # defined as a sequence
+
+        trait :status_approved do
+            status { "approved" }
+        end
+
+        trait :status_rejected do
+            status { "rejected" }
+        end
+
+        trait :status_submitted do
+            status { "submitted" }
+        end
     end
     
 end

f7935240e7cc5ce6a43f8fe7531fe8b162a56588
Zander Pantelick <zpantelick@gmail.com>
2025-02-12 19:05:19 -0800

    Completed dashboard_controller_spec route

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,5 +3,37 @@ require 'rails_helper'
 # GET    /dashboard(.:format)         dashboard#index
 
 RSpec.describe DashboardController, type: :controller do
+    describe "Ticket Controller" do
+        describe "Logged Out:" do
+            let(:user) { FactoryBot.create(:user) }
 
+            describe "GET - /dashboard(.:format) - dashboard#index" do
+                it {
+                    expect(get(:index)).to redirect_to new_user_session_path
+                }
+            end
+        end
+
+        describe "Logged in as User:" do
+            let(:user) { FactoryBot.create(:user) }
+            before(:each) { sign_in user }
+
+            describe "GET - /dashboard(.:format) - dashboard#index" do
+                it {
+                    expect(get(:index)).to be_successful
+                }
+            end
+        end
+
+        describe "Logged in as Admin:" do
+            let(:user) { FactoryBot.create(:user, :admin) }
+            before(:each) { sign_in user }
+
+            describe "GET - /dashboard(.:format) - dashboard#index" do
+                it {
+                    expect(get(:index)).to be_successful
+                }
+            end
+        end
+    end
 end

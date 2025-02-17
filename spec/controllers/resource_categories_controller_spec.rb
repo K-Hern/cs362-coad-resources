dfcb042824e08abe5e73887564a6d51c98983e9a
Kevin Hernandez <KFHEDU@gmail.com>
2025-02-16 20:15:39 -0800

    Adding Resource_category controller spec structure

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -16,6 +16,18 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         describe "Logged Out:" do
             let(:user) { FactoryBot.create(:user) }
 
+            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+                it {
+
+                }
+            end
+
             describe "GET - /resource_categories(.:format) - resource_categories#index" do
                 it {
                     expect(get(:index)).to redirect_to new_user_session_path
@@ -30,8 +42,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
             end
 
             describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it { 
+                it {
                     expect(get(:new)).to redirect_to new_user_session_path
+                }
+            end
+
+            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+                it {
+
+                }
+            end
+
+            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+                it {
+
                 }
             end
         end
@@ -39,6 +81,18 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         describe "Logged in as User:" do
             let(:user) { FactoryBot.create(:user) }
             before(:each) { sign_in user }
+
+            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+                it {
+
+                }
+            end
 
             describe "GET - /resource_categories(.:format) - resource_categories#index" do
                 it {
@@ -54,8 +108,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
             end
 
             describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it { 
+                it {
                     expect(get(:new)).to redirect_to dashboard_path
+                }
+            end
+
+            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+                it {
+
+                }
+            end
+
+            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+                it {
+
                 }
             end
         end
@@ -63,6 +147,18 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         describe "Logged in as Admin:" do
             let(:user) { FactoryBot.create(:user, :admin) }
             before(:each) { sign_in user }
+
+            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+                it {
+
+                }
+            end
 
             describe "GET - /resource_categories(.:format) - resource_categories#index" do
                 it {
@@ -78,8 +174,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
             end
 
             describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it { 
+                it {
                     expect(get(:new)).to be_successful
+                }
+            end
+
+            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+                it {
+
+                }
+            end
+
+            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+                it {
+
+                }
+            end
+
+            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+                it {
+
+                }
+            end
+
+            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+                it {
+
                 }
             end
         end

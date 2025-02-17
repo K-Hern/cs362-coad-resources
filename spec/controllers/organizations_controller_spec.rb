7a028c0db331720e8331419976eb9fa1aea0b2d3
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-02-17 15:33:18 -0800

    logged out user tests complete

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -13,5 +13,101 @@ require 'rails_helper'
 # DELETE /organizations/:id(.:format)             organizations#destroy
 
 RSpec.describe OrganizationsController, type: :controller do
+  describe "Organizations Controller" do
+    describe "Logged Out:" do
+      let(:org) { FactoryBot.build_stubbed(:organization)}
+      let(:user) { FactoryBot.create(:user) }
 
+      describe "POST - /organizations/:id/approve(.:format) - organizations#approve" do
+        it "Redirects to sign in" do
+          post(:approve, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "POST - /organizations/:id/reject(.:format) - organizations#reject" do
+        it "Redirects to sign in" do
+          post(:reject, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+
+      #resources is not a method in the organizations controller - why is this in the routes?
+
+      #describe "GET - /organizations/:id/resources(.:format) - organizations#resources" do
+      #  it "Redirects to sign in" do
+      #    get(:resources, params: {id: org.id})
+      #    expect(response).to redirect_to new_user_session_path
+      #  end
+      #end
+
+      describe "GET - /organizations(.:format) - organizations#index" do
+        it "Redirects to sign in" do
+          get(:index)
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "POST - /organizations(.:format) - organizations#create" do
+        it "Redirects to sign in" do
+          post(:create)
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /organizations/new(.:format) - organizations#new" do
+        it "Redirects to sign in" do
+          get(:new)
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /organizations/:id/edit(.:format) - organizations#edit" do
+        it "Redirects to sign in" do
+          get(:edit, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /organizations/:id(.:format) - organizations#show" do
+        it "Redirects to sign in" do
+          get(:show, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "PATCH - /organizations/:id(.:format) - organizations#update" do
+        it "Redirects to sign in" do
+          patch(:update, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "PUT - /organizations/:id(.:format) - organizations#update" do
+        it "Redirects to sign in" do
+          put(:update, params: {id: org.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+
+      #destroy is not a method in the organizations controller - why is this in the routes?
+
+      #describe "DELETE - /organizations/:id(.:format) - organizations#destroy" do
+      #  it "Redirects to sign in" do
+      #    delete(:destroy, params: {id: org.id})
+      #    expect(response).to redirect_to new_user_session_path
+      #  end
+      #end
+
+    end
+
+    describe "Logged in as User (Non-Admin):" do
+      let(:user) { FactoryBot.create(:user) }
+      before(:each) { sign_in user }
+
+      
+    end
+  end
 end

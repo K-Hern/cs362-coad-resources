a7a5b1ecedefa927e15de7ca5e7bef8a63772304
Kevin Hernandez <KFHEDU@gmail.com>
2025-02-16 22:03:56 -0800

    Completed Logged out resource_categories_controller spec

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -12,202 +12,211 @@ require 'rails_helper'
 # DELETE /resource_categories/:id(.:format)                   resource_categories#destroy
 
 RSpec.describe ResourceCategoriesController, type: :controller do
-    describe "Resource Categories Controller" do
-        describe "Logged Out:" do
-            let(:user) { FactoryBot.create(:user) }
+  describe "Resource Categories Controller" do
+    describe "Logged Out:" do
+      let(:resource_cat) { FactoryBot.create(:resource_category)}
+      let(:user) { FactoryBot.create(:user)}
 
-            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#index" do
-                it {
-                    expect(get(:index)).to redirect_to new_user_session_path
-                }
-            end
-
-            describe "POST - /resource_categories(.:format) - resource_categories#create" do
-                it {
-                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
-                    expect(response).to redirect_to new_user_session_path
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it {
-                    expect(get(:new)).to redirect_to new_user_session_path
-                }
-            end
-
-            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
-                it {
-
-                }
-            end
+      describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+        it "Redirects to sign in" do
+          patch(:activate, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
         end
+      end
 
-        describe "Logged in as User:" do
-            let(:user) { FactoryBot.create(:user) }
-            before(:each) { sign_in user }
-
-            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#index" do
-                it {
-                    expect(get(:index)).to redirect_to dashboard_path
-                }
-            end
-
-            describe "POST - /resource_categories(.:format) - resource_categories#create" do
-                it {
-                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
-                    expect(response).to redirect_to dashboard_path
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it {
-                    expect(get(:new)).to redirect_to dashboard_path
-                }
-            end
-
-            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
-                it {
-
-                }
-            end
+      describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+        it "Redirects to sign in" do
+          patch(:deactivate, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
         end
+      end
 
-        describe "Logged in as Admin:" do
-            let(:user) { FactoryBot.create(:user, :admin) }
-            before(:each) { sign_in user }
-
-            describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#index" do
-                it {
-                    expect(get(:index)).to be_successful
-                }
-            end
-
-            describe "POST - /resource_categories(.:format) - resource_categories#create" do
-                it {
-                    post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
-                    expect(response).to redirect_to resource_categories_path
-                }
-            end
-
-            describe "GET - /resource_categories(.:format) - resource_categories#new" do
-                it {
-                    expect(get(:new)).to be_successful
-                }
-            end
-
-            describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
-                it {
-
-                }
-            end
-
-            describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
-                it {
-
-                }
-            end
-
-            describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
-                it {
-
-                }
-            end
-
-            describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
-                it {
-
-                }
-            end
+      describe "GET - /resource_categories(.:format) - resource_categories#index" do
+        it "Redirects to sign in" do
+          get(:index)
+          expect(response).to redirect_to new_user_session_path
         end
+      end
+
+      describe "POST - /resource_categories(.:format) - resource_categories#create" do
+        it "Redirects to sign in" do
+          post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /resource_categories(.:format) - resource_categories#new" do
+        it "Redirects to sign in" do
+          get(:new)
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+        it "Redirects to sign in" do
+          get(:edit, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+        it "Redirects to sign in" do
+          get(:show, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+        it "Redirects to sign in" do
+          patch(:update, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+        it "Redirects to sign in" do
+          put(:update, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+
+      describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+        it "Redirects to sign in" do
+          delete(:destroy, params: {id: resource_cat.id})
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
     end
+
+    describe "Logged in as User (Non-Admin):" do
+      let(:user) { FactoryBot.create(:user) }
+      before(:each) { sign_in user }
+
+      # describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+      #   it {
+
+      #   }
+      # end
+
+      # describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+      #   it {
+      #   }
+      # end
+
+      describe "GET - /resource_categories(.:format) - resource_categories#index" do
+        it {
+          expect(get(:index)).to redirect_to dashboard_path
+        }
+      end
+
+      describe "POST - /resource_categories(.:format) - resource_categories#create" do
+        it {
+          post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+          expect(response).to redirect_to dashboard_path
+        }
+      end
+
+      describe "GET - /resource_categories(.:format) - resource_categories#new" do
+        it {
+          expect(get(:new)).to redirect_to dashboard_path
+        }
+      end
+
+      # describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+      #   it {
+
+      #   }
+      # end
+
+      # describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+      #   it {
+
+      #   }
+      # end
+
+      # describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+      #   it {
+
+      #   }
+      # end
+
+      # describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+      #   it {
+
+      #   }
+      # end
+
+      # describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+      #   it {
+
+      #   }
+      # end
+    end
+
+    describe "Logged in as Admin:" do
+      let(:user) { FactoryBot.create(:user, :admin) }
+      before(:each) { sign_in user }
+
+      # describe "PATCH - /resource_categories/:id/activate(.:format) - resource_categories#activate" do
+      #     it {
+
+      #     }
+      # end
+
+      # describe "PATCH - /resource_categories/:id/deactivate(.:format) - resource_categories#deactivate" do
+      #     it {
+
+      #     }
+      # end
+
+      describe "GET - /resource_categories(.:format) - resource_categories#index" do
+          it {
+              expect(get(:index)).to be_successful
+          }
+      end
+
+      describe "POST - /resource_categories(.:format) - resource_categories#create" do
+          it {
+              post(:create, params: { resource_category: FactoryBot.attributes_for(:resource_category) })
+              expect(response).to redirect_to resource_categories_path
+          }
+      end
+
+      describe "GET - /resource_categories(.:format) - resource_categories#new" do
+          it {
+              expect(get(:new)).to be_successful
+          }
+      end
+
+      # describe "GET - /resource_categories/:id/edit(.:format) - resource_categories#edit" do
+      #     it {
+
+      #     }
+      # end
+
+      # describe "GET - /resource_categories/:id(.:format) - resource_categories#show" do
+      #     it {
+
+      #     }
+      # end
+
+      # describe "PATCH - /resource_categories/:id(.:format) - resource_categories#update" do
+      #     it {
+
+      #     }
+      # end
+
+      # describe "PUT - /resource_categories/:id(.:format) - resource_categories#update" do
+      #     it {
+
+      #     }
+      # end
+
+      # describe "DELETE - /resource_categories/:id(.:format) - resource_categories#destroy" do
+      #     it {
+
+      #     }
+      # end
+    end
+  end
 end

e4fa12013d51b5f9b632b0da408389c3442d00e3
Kevin Hernandez <KFHEDU@gmail.com>
2025-02-19 13:37:43 -0800

    Adding Empty Structure to OrganizationsController Spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -32,9 +32,7 @@ RSpec.describe OrganizationsController, type: :controller do
         end
       end
 
-
-      #resources is not a method in the organizations controller - why is this in the routes?
-
+      # Resources is not a method in the organizations controller - why is this in the routes?
       #describe "GET - /organizations/:id/resources(.:format) - organizations#resources" do
       #  it "Redirects to sign in" do
       #    get(:resources, params: {id: org.id})
@@ -108,13 +106,129 @@ RSpec.describe OrganizationsController, type: :controller do
       let(:user) { FactoryBot.create(:user) }
       before(:each) { sign_in user }
 
+      describe "POST - /organizations/:id/approve(.:format) - organizations#approve" do
+        it "" do
+        end
+      end
+
+      describe "POST - /organizations/:id/reject(.:format) - organizations#reject" do
+        it "" do
+        end
+      end
+
+      # Resources is not a method in the organizations controller - why is this in the routes?
+      describe "GET - /organizations/:id/resources(.:format) - organizations#resources" do
+        it "" do
+        end
+      end
+
       describe "GET - /organizations(.:format) - organizations#index" do
         it "Serves dashboard" do
           get(:index)
           expect(response).to be_successful
         end
       end
-      
+
+      describe "POST - /organizations(.:format) - organizations#create" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/new(.:format) - organizations#new" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/:id/edit(.:format) - organizations#edit" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/:id(.:format) - organizations#show" do
+        it "" do
+        end
+      end
+
+      describe "PATCH - /organizations/:id(.:format) - organizations#update" do
+        it "" do
+        end
+      end
+
+      describe "PUT - /organizations/:id(.:format) - organizations#update" do
+        it "" do
+        end
+      end
+
+      # Destroy is not a method in the organizations controller - why is this in the routes?
+      describe "DELETE - /organizations/:id(.:format) - organizations#destroy" do
+        it "" do
+        end
+      end
+    end
+
+    describe "Logged in as Admin:" do
+      let(:org) { FactoryBot.create(:organization)}
+      let(:user) { FactoryBot.create(:user, :admin) }
+      before(:each) { sign_in user }
+
+      describe "POST - /organizations/:id/approve(.:format) - organizations#approve" do
+        it "" do
+        end
+      end
+
+      describe "POST - /organizations/:id/reject(.:format) - organizations#reject" do
+        it "" do
+        end
+      end
+
+      # Resources is not a method in the organizations controller - why is this in the routes?
+      describe "GET - /organizations/:id/resources(.:format) - organizations#resources" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations(.:format) - organizations#index" do
+        it "" do
+
+        end
+      end
+
+      describe "POST - /organizations(.:format) - organizations#create" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/new(.:format) - organizations#new" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/:id/edit(.:format) - organizations#edit" do
+        it "" do
+        end
+      end
+
+      describe "GET - /organizations/:id(.:format) - organizations#show" do
+        it "" do
+        end
+      end
+
+      describe "PATCH - /organizations/:id(.:format) - organizations#update" do
+        it "" do
+        end
+      end
+
+      describe "PUT - /organizations/:id(.:format) - organizations#update" do
+        it "" do
+        end
+      end
+
+      # Destroy is not a method in the organizations controller - why is this in the routes?
+      describe "DELETE - /organizations/:id(.:format) - organizations#destroy" do
+        it "" do
+        end
+      end
+
     end
   end
 end

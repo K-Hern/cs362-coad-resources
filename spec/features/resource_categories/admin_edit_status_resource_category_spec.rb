6d0b2a0a4cfbbaac34bd4f52293e00938cb6e08b
Kevin Hernandez <KFHEDU@gmail.com>
2025-03-09 16:52:25 -0700

    Add Resource category edit activation status feature spec

--- spec/features/resource_categories/admin_edit_status_resource_category_spec.rb
+++ spec/features/resource_categories/admin_edit_status_resource_category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
+RSpec.describe 'Edit a Resource Category Status', type: :feature do
 
-RSpec.describe 'Deleting a Resource Category', type: :feature do
+  let(:categories_path) {'/resource_categories'}
 
+  before(:each) {
+    @user = FactoryBot.create(:user, :admin)
+    log_in_as(@user)
+  }
+
+  describe "Category Status Edit" do
+    it "Allows the activation of an existing Resource Category" do
+      FactoryBot.create(:resource_category, name: "Test Category", active: false)
+      visit categories_path
+
+      click_on "Test Category"
+      click_on "Activate"
+
+      expect(page).to have_text("Category activated.")
+    end
+
+    it "Allows the activation of an existing Resource Category" do
+      FactoryBot.create(:resource_category, name: "Test Category", active: true)
+      visit categories_path
+
+      click_on "Test Category"
+      click_on "Deactivate"
+
+      expect(page).to have_text("Category deactivated.")
+    end
+  end
 end

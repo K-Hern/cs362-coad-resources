b0884d9cfeb67f0215cb289b78883f6ef29a7beb
Kevin Hernandez <KFHEDU@gmail.com>
2025-03-09 05:39:58 -0700

    Finished regions creation and dashboard access feature tests

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -4,36 +4,66 @@ require 'rails_helper'
 RSpec.describe 'Region Feature Tests', type: :feature do
 
   let(:region_path) {'/regions'}
+  let(:tickets_path) {'/tickets'}
 
   before(:each) {
     @user = FactoryBot.create(:user, :admin)
     log_in_as(@user)
   }
 
-  it "Allows the creation of a new region" do
-    visit region_path
-    expect(current_path).to eq region_path
+  describe "Region Creation" do
+    it "Allows the creation of a new region" do
+      visit region_path
+      expect(current_path).to eq region_path
 
-    click_on "Add Region"
-    fill_in "region_name", with: "Test Region"
-    click_on "Add Region"
+      click_on "Add Region"
+      fill_in "region_name", with: "Test Region"
+      click_on "Add Region"
 
-    expect(current_path).to eq region_path
-    expect(page).to have_selector('div.alert.alert-dismissible.fade.show.alert-success', text: 'Region successfully created.')
+      expect(current_path).to eq region_path
+      expect(page).to have_selector('div.alert.alert-dismissible.fade.show.alert-success', text: 'Region successfully created.')
+    end
+
+    it "Does not allow the creation of a duplicate region" do
+      FactoryBot.create(:region, name: "Test Region")
+
+      visit region_path
+      expect(current_path).to eq region_path
+
+      click_on "Add Region"
+      fill_in "region_name", with: "Test Region"
+      click_on "Add Region"
+
+      expect(current_path).to eq region_path
+      expect(page).to have_selector('#error_explanation', text: "1 error prohibited this region from being saved:\nName has already been taken")
+    end
   end
 
-  it "Does not allow the creation of a duplicate region" do
-    FactoryBot.create(:region, name: "Test Region")
+  describe "Region Dashboard" do
+    it "shows the created regions as a list" do
+      FactoryBot.create(:region, name: "Test Region")
+      FactoryBot.create(:region, name: "Test Region 2")
 
-    visit region_path
-    expect(current_path).to eq region_path
+      visit region_path
+      expect(current_path).to eq region_path
 
-    click_on "Add Region"
-    fill_in "region_name", with: "Test Region"
-    click_on "Add Region"
+      expect(page).to have_text("Test Region")
+      expect(page).to have_text("Test Region 2")
 
-    expect(current_path).to eq region_path
-    expect(page).to have_selector('#error_explanation', text: "1 error prohibited this region from being saved:\nName has already been taken")
+    end
+
+    it "Shows the tickets associated with the region" do
+      @region = FactoryBot.create(:region, name: "Test Region")
+      ticket = FactoryBot.create(:ticket, name: "Test Ticket", region: @region)
+
+      visit region_path
+      expect(current_path).to eq region_path
+
+      click_on "Test Region"
+      expect(page).to have_text("Test Ticket")
+      click_on "Ticket 1"
+      expect(current_path).to eq "#{tickets_path}/#{ticket.id}"
+
+    end
   end
-
 end

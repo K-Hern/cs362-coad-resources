47640b8bdfa48877c362931e65fe4cea8fe08d81
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 01:38:52 -0700

    Add feature test for organization application signup process

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
-
+    it "allows a new organization to apply for help" do
+        visit root_path
+        click_on "Give Help"
+        click_on "Apply Now"
+        
+        fill_in 'Email', with: 'org@example.com'
+        fill_in 'Password', with: 'password123'
+        fill_in 'Password confirmation', with: 'password123'
+        
+        click_button "Sign up"
+        expect(page).to have_text("A message with a confirmation link")
+    end
 end

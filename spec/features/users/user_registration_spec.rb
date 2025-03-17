36b2eb38b2eac505662a6b8eb13a6e53009773f6
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 00:37:46 -0700

    Add feature test for user registration process

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+    let(:signup_path) { '/signup' }
 
+    describe "Registration process" do
+        it "allows a new user to sign up" do
+            visit signup_path
+            expect(current_path).to eq '/signup'
+
+            fill_in 'Email', with: 'test@example.com'
+            fill_in 'Password', with: 'password123'
+            fill_in 'Password confirmation', with: 'password123'
+            
+            click_button "Sign up"
+            expect(page).to have_text("A message with a confirmation link has been sent to your email address.")
+        end
+    end
 end

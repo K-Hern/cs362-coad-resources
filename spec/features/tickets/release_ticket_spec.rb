3d2c2a76cbe8c1814b2223271fb3dd250e0c5306
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 01:39:15 -0700

    Add feature test for releasing assigned tickets by approved users

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'Releasing a ticket by an', type: :feature do
+RSpec.describe 'Releasing a ticket', type: :feature do
+    let(:dashboard_path) { '/dashboard' }
 
+    before(:each) do
+        @user = FactoryBot.create(:user, :organization_approved)
+        log_in_as(@user)
+        @ticket = FactoryBot.create(:ticket, :captured, organization_id: @user.id)
+    end
+
+    describe "Ticket Release" do
+        it "allows a user to release an assigned ticket" do
+            visit dashboard_path
+            expect(current_path).to eq dashboard_path
+
+            click_on "Tickets"
+            click_on @ticket.name
+            expect(current_path).to eq "/tickets/#{@ticket.id}"
+
+            click_on "Release"
+            expect(current_path).to eq dashboard_path
+        end
+    end
 end

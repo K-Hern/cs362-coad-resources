fef8fd06d2dc4be6476a2aa06fc9aa9450a977ae
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 01:39:06 -0700

    Add feature test for capturing unassigned tickets by approved users

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Capturing a ticket', type: :feature do
+RSpec.describe 'Capturing a Ticket', type: :feature do
+    let(:dashboard_path) { '/dashboard' }
 
+    before(:each) {
+        @user = FactoryBot.create(:user, :organization_approved)
+        log_in_as(@user)
+        @ticket = create(:ticket)
+    }
+
+    describe "Ticket Capture" do
+        it "allows a user to capture an unassigned ticket" do
+            visit dashboard_path
+            expect(current_path).to eq dashboard_path
+                  
+            click_on "Tickets"
+ 
+            click_on @ticket.name
+            expect(current_path).to eq "/tickets/#{@ticket.id}"
+            
+            click_on "Capture"
+            expect(current_path).to eq dashboard_path
+        end
+    end
 end

66e48845b47f4d12ac4728ce51265119bfa1041a
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 00:37:41 -0700

    Add feature test for user login functionality

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-
+    let(:user) { FactoryBot.create(:user, email: 'user@example.com', password: 'password123') }
+  
+    it "allows a user to sign in" do
+        visit '/login'
+        expect(current_path).to eq('/login')
+      
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_button 'Sign in'
+      
+        expect(page).to have_text("Signed in successfully.")
+    end
 end

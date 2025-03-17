faf3625678031ca441c0f54eb8415c797550c6b7
Zander Pantelick <zpantelick@gmail.com>
2025-03-17 01:38:46 -0700

    Add feature test for admin organization approval process

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'Approving an organization', type: :feature do
-
+RSpec.describe 'Approving an organization', type: :feature do  
+    before(:each) do
+        @user = FactoryBot.create(:user, :admin)
+        log_in_as(@user)
+        @org = create(:organization, name: "Pending Org")
+    end
+  
+    it "allows an admin to approve a submitted organization" do
+        visit '/organizations'
+        click_on "All"
+      
+        click_on @org.name
+      
+        expect(current_path).to eq "/organizations/#{@org.id}"
+      
+        click_on "Approve"
+      
+        expect(page).to have_text("Organization #{@org.name} has been approved")
+    end
 end
